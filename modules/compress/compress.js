f916c9591f928e9c829e928a3a5946b3c45f8f18
Alexey Krukovich <Alexey.Krukovich@inspectorio.com>
2022-06-08 15:01:59 +0300

    refactor: change the archive extensions

--- modules/compress/compress.js
+++ modules/compress/compress.js
@@ -7,9 +7,8 @@ export const compressFile = async (options) => {
   const [inputPath, outputPath] = options;
   const fileName = path.basename(inputPath);
   const readStream = fs.createReadStream(fileName);
-  const writeStream = fs.createWriteStream(`${outputPath}/${fileName}.gz`);
+  const writeStream = fs.createWriteStream(`${outputPath}/${fileName}.br`);
   const brotli = zlib.createBrotliCompress();
-
 
   readStream.on('error', () => showOperationError());
   writeStream.on('error', () => showOperationError());
